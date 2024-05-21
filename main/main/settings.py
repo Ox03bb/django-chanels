bdf286725d2c4dfb4caca76091764d138fd6ff63
0x03bb <alibouakaz91@gmail.com>
2024-05-21 23:32:55 +0100

    chat is work

--- main/main/settings.py
+++ main/main/settings.py
@@ -44,6 +44,14 @@ INSTALLED_APPS = [
 # mysite/settings.py
 # Daphne
 ASGI_APPLICATION = "main.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
